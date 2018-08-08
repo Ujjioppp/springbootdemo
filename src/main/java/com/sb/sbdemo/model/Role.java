2e6d76027915f51d9cf6e08c3d0aea4db47605de
yl <3253942@qq.com>
2018-08-08 16:43:25 +0800

    Role

---
+++ src/main/java/com/sb/sbdemo/model/Role.java
@@ -0,0 +1,36 @@
+package com.sb.sbdemo.model;
+
+/**
+ * Created by ye on 8/8/18.
+ */
+public class Role{
+    private Long id;
+
+    private String code;
+
+    private String desc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
